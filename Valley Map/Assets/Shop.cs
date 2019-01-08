082d0c523f7f12087b3ac915f6cb3f49d2e2b1d7
Sergey Rudenko <hzzh@outlook.com>
2019-01-08 21:56:03 +0200

    shop video added

---
+++ Valley Map/Assets/Shop.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Shop : MonoBehaviour
+{
+	public GameObject videoPlayer;
+	public int timeToStop;
+	bool flag = true;
+    void Start()
+    {
+		videoPlayer.SetActive (false);
+    }
+
+	void OnTriggerEnter(Collider player)
+	{
+		if (flag) {
+			videoPlayer.SetActive (true);
+			Destroy (videoPlayer, timeToStop);
+			flag = false;
+		}
+	}
+}
