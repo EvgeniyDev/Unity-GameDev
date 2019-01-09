806ab2c47be87bb31cbe4e3e36595117147911e1
Sergey Rudenko <hzzh@outlook.com>
2019-01-09 14:42:02 +0200

    inventory

--- Valley Map/Assets/Shop.cs
+++ Valley Map/Assets/Shop.cs
@@ -2,23 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class Shop : MonoBehaviour
 {
 	public GameObject videoPlayer;
 	public int timeToStop;
-	bool flag = true;
+	int flag = 0;
+	public FirstPersonController fps;
     void Start()
     {
 		videoPlayer.SetActive (false);
     }
-
-	void OnTriggerEnter(Collider player)
+		
+	void OnTriggerEnter()
 	{
-		if (flag) {
+		if (flag == 0) {
+			//fps.GetComponent<MouseLook>().XSensitivity = 0;
+			//this.gameObject.GetComponent<MouseLook>().YSensitivity = 0;
 			videoPlayer.SetActive (true);
 			Destroy (videoPlayer, timeToStop);
-			flag = false;
+			flag = 1;
+		}
+		if (flag == 1) {
+			//this.gameObject.GetComponent<MouseLook>().XSensitivity = 2;
+			//this.gameObject.GetComponent<MouseLook>().YSensitivity = 2;
+			inventOn = true;
+			flag = 2;
 		}
 	}
+		
+	bool inventOn = false;
+	void OnTriggerStay(){
+		if (Input.GetKey (KeyCode.E)) {
+			if (inventOn) {
+				//shopInventory.enabled = false;
+			} else {
+				//shopInventory.enabled = true;
+			}
+			inventOn = !inventOn;
+		}
+	}
+
 }
