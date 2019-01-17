4e91ef4f010fbdc9bbc35ad1d543aca14df3963c
Sergey Rudenko <hzzh@outlook.com>
2019-01-17 17:23:44 +0200

    invent fix

--- Valley Map/Assets/Imported materials/Shop/Invent/Items.cs
+++ Valley Map/Assets/Imported materials/Shop/Invent/Items.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class Items : MonoBehaviour
 {
@@ -8,20 +9,36 @@ public class Items : MonoBehaviour
 	public int mouseSlot;
 	public ShopScript lib;
 
+
+	private bool invent = false;
+
+
+
+	void Update(){
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			invent = !invent;
+			Time.timeScale = 1;
+		}
+	}
+
+
+
 	void OnGUI()
 	{
-		for (int x = 0; x < 5; x++) 
-		{
-			for (int y = 0; y < 5; y++)
+		if (invent) {
+			for (int x = 0; x < 5; x++) 
 			{
-				if (GUI.Button (new Rect (x * 100, y * 100, 100, 100), lib.Images [items[y*5+x]])) {
-					int loc = items [y * 5 + x];
-					items [y * 5 + x] = mouseSlot;
-					mouseSlot = loc;
+				for (int y = 0; y < 5; y++)
+				{
+					if (GUI.Button (new Rect (x * 100, y * 100, 100, 100), lib.Images [items[y*5+x]])) {
+						int loc = items [y * 5 + x];
+						items [y * 5 + x] = mouseSlot;
+						mouseSlot = loc;
+					}
 				}
 			}
+			GUI.DrawTexture (new Rect (Input.mousePosition.x, Screen.height - Input.mousePosition.y, 100, 100), lib.Images [mouseSlot]);
 		}
-		GUI.DrawTexture (new Rect (Input.mousePosition.x, Screen.height - Input.mousePosition.y, 100, 100), lib.Images [mouseSlot]);
-
 	}
 }
