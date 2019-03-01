c977b6a977acad968a7cad0194d09f27da7c3187
Sergey Rudenko <hzzh@outlook.com>
2019-03-01 19:08:43 +0200

    new scene + trigger beta

---
+++ MGTOWn/Assets/Level and character/Scenes/BetweenScenes.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BetweenScenes : MonoBehaviour
+{
+    // Start is called before the first frame update
+	void OnTriggerEnter(Collider other){
+		SceneManager.LoadScene("Assets/Level and character/Scenes/Main level.unity");
+		//SceneManager.LoadScene();
+	}
+}
