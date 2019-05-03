db86c20df9f245adaa27d3e96f71c1c4c70e9bd7
Peasoon <zh.hmelevskoj2013@gmail.com>
2019-05-03 18:32:26 +0300

    Mob animation part 1
    - Animate Golem walk;
    
    Also fixed size of the pick up message window;

---
+++ MGTOWn/Assets/Resources/Golem/GolemScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GolemScript : MonoBehaviour
+{
+    Animator animator;    
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();      
+    }
+
+    void Update()
+    {
+        transform.Translate(new Vector3(0, 0, 1.5f) * Time.deltaTime);
+        
+    }
+
+}
