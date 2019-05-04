12e37db3086acb0ce5960c64da957d403a9a40af
unknown <hzzh@outlook.com>
2019-05-04 17:01:19 +0300

    woman animation
    Women walking animation, same forall

---
+++ MGTOWn/Assets/Level and character/WoMen/womanScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class womanScript : MonoBehaviour
+{
+	Animator animator;    
+
+	void Start()
+	{
+		animator = GetComponent<Animator>();      
+	}
+
+	void Update()
+	{
+		transform.Translate(new Vector3(0, 0, 1.2f) * Time.deltaTime);
+
+	}
+}
