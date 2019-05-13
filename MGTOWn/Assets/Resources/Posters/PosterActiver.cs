6568e3f37ca03283b608b53f6192dc030dfe16a1
unknown <hzzh@outlook.com>
2019-05-13 07:03:30 +0300

    Posters setter
    hint on screen center & posters setting by plane texture change

---
+++ MGTOWn/Assets/Resources/Posters/PosterActiver.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PosterActiver : MonoBehaviour
+{
+	public TextMeshProUGUI Hint;
+	public GameObject Poster;
+	public Texture2D texture;
+	private Vector3 rayOrigin;
+	private RaycastHit hit;
+	private bool isPosted = false;
+
+	void Start()
+	{
+		rayOrigin = new Vector3(0.5f, 0.5f, 0f); // center of the screen
+		//texture = (Texture2D) Resources.Load ("Assets/Resources/Posters/MGTOWN0"); //poster
+	}
+
+	void OnTriggerExit (Collider col) 
+	{
+		Hint.text = "";
+	}
+	void OnTriggerStay (Collider col) 
+	{
+		if (!isPosted) {
+			Ray ray = Camera.main.ViewportPointToRay (rayOrigin);
+			if (Physics.Raycast (ray, out hit, 5.0f)) {
+				if (hit.collider.tag == Poster.tag) {
+					Hint.text = "Press E to put poster";
+					if (Input.GetKeyDown (KeyCode.E)) {
+						Poster.GetComponent<Renderer> ().material.mainTexture = texture;
+						isPosted = true;
+						Hint.text = "";
+					}
+				} else
+					Hint.text = "";
+			} else
+				Hint.text = "";
+		} else
+			Hint.text = "";
+	}
+
+}
