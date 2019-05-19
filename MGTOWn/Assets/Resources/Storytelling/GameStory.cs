2b78443ff93805b75d22354beb58972b40101b86
Peasoon <zh.hmelevskoj2013@gmail.com>
2019-05-19 22:11:54 +0300

    Теперь работает
    Устранена ошибка отсутствия ссылок на объекты в дочернем классе.
    Переделана архитектура квестов: класс квестов теперь не абстрактный и
    вынесен в отдельный файл, в классе квест 1 методе void Start()
    вызывается родительская функция класса Quests.
    
    Вероятнее всего поехал layout сценария игры из-за частичной установки
    относительных размеров элементов UI.

--- MGTOWn/Assets/Resources/Storytelling/GameStory.cs
+++ MGTOWn/Assets/Resources/Storytelling/GameStory.cs
@@ -1,106 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Data;
-using Mono.Data.Sqlite;
-using System.IO;
-using UnityEngine;
-using System.Threading;
-using UnityEngine.UI;
-using UnityStandardAssets.Characters.FirstPerson;
+﻿using UnityEngine;
 
 public class GameStory : MonoBehaviour
 {
-	public Text longText; 
-	public GameObject fps;
-	public GameObject UI_Player;
-	public GameObject UI_PauseMenu;
-	public GameObject UI_Author;
-	public GameObject UI_Dialog;
-
-    void Start()
+    public void Start()
     {
-		Debug.Log ("Started");
-		new Quest1().start();
-		Debug.Log ("Started");
+        Debug.Log ("Started");
+		new Quest1().Start();
     }
-
-	void Update()
-	{
-		
-	}
-
-	public void DisableFPS() { 
-		//fps.GetComponent<FirstPersonController> ().enabled = false; 
-	}
-	public void EnableFPS() { 
-		//fps.GetComponent<FirstPersonController> ().enabled = true; 
-	}
-	public void SetLongAuthorText(string AuthorText){
-		for (int i = 0; i < AuthorText.Length; i++)
-			longText.text += AuthorText[i];
-	}
 }
-
-
-
-
-
-abstract class Quests : GameStory{
-
-	string connection;
-	protected IDbConnection dbcon;
-	protected IDbCommand cmnd_read;
-	protected IDataReader reader;
-	protected string query;
-
-	public enum LayoutUI{ Author, Dialog, NULL }
-
-	public Quests(){
-		connection = "URI=file:" + Application.dataPath + "/Resources/Storytelling/AllContent.db";
-		dbcon = new SqliteConnection(connection);
-		dbcon.Open ();
-		cmnd_read = dbcon.CreateCommand();
-	}
-
-	/*
-	public void ActiveScenarioLayout(bool flag, LayoutUI layoutType = LayoutUI.NULL){
-		// true - only scenario
-		// false - disable scenario
-		if (flag) {
-			DisableFPS ();
-			UI_PauseMenu.SetActive(false);
-			UI_Player.SetActive (false);
-			if (layoutType == LayoutUI.Author) {
-				UI_Dialog.SetActive (false);
-				UI_Author.SetActive (true);
-			}
-			if (layoutType == LayoutUI.Dialog) {
-				UI_Dialog.SetActive (true);
-				UI_Author.SetActive (false);
-			}
-		} else {
-			EnableFPS ();
-			UI_PauseMenu.SetActive (true);
-			UI_Player.SetActive (true);
-			UI_Author.SetActive (false);
-			UI_Dialog.SetActive (false);
-		}
-	}
-*/
-	public abstract void start ();
-}
-
-
 
 
 
 class Quest1 : Quests{
 
-	public override void start ()
+	public override void Start ()
 	{
+        base.Start();
+
 		Debug.Log ("Quest 1");
 
-		//ActiveScenarioLayout (true, LayoutUI.Author);
+		ActiveScenarioLayout (true, LayoutUI.Author);
 
 		query = "SELECT * FROM Phrases WHERE id_quest = 0;";
 		cmnd_read.CommandText = query;
@@ -113,7 +32,7 @@ class Quest1 : Quests{
 
 		dbcon.Close ();
 
-		//ActiveScenarioLayout (false, LayoutUI.NULL);
+		ActiveScenarioLayout (false, LayoutUI.NULL);
 	}
 
 }
---
+++ MGTOWn/Assets/Resources/Storytelling/Quests.cs
@@ -0,0 +1,94 @@
+﻿using System.Data;
+using Mono.Data.Sqlite;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+
+class Quests : MonoBehaviour
+{
+    string connection;
+    protected IDbConnection dbcon;
+    protected IDbCommand cmnd_read;
+    protected IDataReader reader;
+    protected string query;
+
+    Text longText;
+    GameObject fps;
+    GameObject UI_Player;
+    GameObject UI_PauseMenu;
+    GameObject scenarioLayout;
+    GameObject UI_Author;
+    GameObject UI_Dialog;
+
+    public enum LayoutUI { Author, Dialog, NULL }
+
+    public void ActiveScenarioLayout(bool flag, LayoutUI layoutType = LayoutUI.NULL)
+    {
+        // true - only scenario
+        // false - disable scenario
+        if (flag)
+        {
+            DisableFPS();
+            UI_PauseMenu.SetActive(false);
+            UI_Player.SetActive(false);
+            if (layoutType == LayoutUI.Author)
+            {
+                UI_Dialog.SetActive(false);
+                UI_Author.SetActive(true);
+            }
+            if (layoutType == LayoutUI.Dialog)
+            {
+                UI_Dialog.SetActive(true);
+                UI_Author.SetActive(false);
+            }
+        }
+        else
+        {
+            EnableFPS();
+            UI_PauseMenu.SetActive(true);
+            UI_Player.SetActive(true);
+            UI_Author.SetActive(false);
+            UI_Dialog.SetActive(false);
+        }
+    }
+
+    public void DisableFPS()
+    {
+        fps.GetComponent<FirstPersonController>().enabled = false;
+    }
+
+    public void EnableFPS()
+    {
+        fps.GetComponent<FirstPersonController>().enabled = true;
+    }
+
+    public void SetLongAuthorText(string AuthorText)
+    {
+        for (int i = 0; i < AuthorText.Length; i++)
+            longText.text += AuthorText[i];
+    }
+
+    public virtual void Start()
+    {
+        fps = GameObject.FindGameObjectWithTag("Player");
+        UI_Player = fps.transform.GetChild(2).gameObject;
+        UI_PauseMenu = fps.transform.GetChild(3).gameObject;
+
+        scenarioLayout = fps.transform.GetChild(4).gameObject;
+        UI_Author = scenarioLayout.transform.GetChild(0).gameObject;
+        UI_Dialog = scenarioLayout.transform.GetChild(1).gameObject;
+
+        longText = UI_Author.transform.GetChild(1).GetComponent<Text>();
+
+
+        connection = "URI=file:" + Application.dataPath + "/Resources/Storytelling/AllContent.db";
+        dbcon = new SqliteConnection(connection);
+        dbcon.Open();
+        cmnd_read = dbcon.CreateCommand();
+    }
+}
+
+
+
+
+
