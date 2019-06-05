54af6a86dc29b7a73872e18530a181b4b1f434d9
Peasoon <zh.hmelevskoj2013@gmail.com>
2019-06-05 15:19:34 +0300

    Fightig part 4
    Added NavMeshAgent and scripted logic a bit to the spider model. Now it
    can pursue a player.

---
+++ MGTOWn/Assets/Enemies/EnemyController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public float lookRadius = 15f;
+
+    Transform target;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        float distanceToTarget = Vector3.Distance(target.position, transform.position);
+
+        if (distanceToTarget <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+
+            if (distanceToTarget <= agent.stoppingDistance)
+            {
+                //Attack
+
+                FaceTarget();
+            }
+        }
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f); 
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
